3b23e48d25210774a58ab5c4a85a949f19dd6a23
Grzegorz <gchmura3@gmail.com>
2017-04-10 12:02:09 +0200

    Registration Validation

--- CompetitionRegistration/ViewModels/CompetitorRegistrationViewModel.cs
+++ CompetitionRegistration/ViewModels/CompetitorRegistrationViewModel.cs
@@ -10,13 +10,19 @@ namespace CompetitionRegistration.ViewModels
     public class CompetitorRegistrationViewModel
     {
         [Required(ErrorMessage = "Please insert First Name")]
+        [Display(Name = "Name")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please insert Last Name")]
+        [Display(Name = "Surname")]
         public string LastName { get; set; }
-
+        
+        [Required(ErrorMessage = "Please choose Sex")]
+      
         public string Sex { get; set; }
+        [Required(ErrorMessage = "Please choose Birth Date")]
+        [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
-        [Required(ErrorMessage = "Proszę podać adres e-mail.")]
+        [Required(ErrorMessage = "Please insert  e-mail address.")]
         [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Proszę podać prawidłowy adres e-mail.")]
         public string Email { get; set; }
        
