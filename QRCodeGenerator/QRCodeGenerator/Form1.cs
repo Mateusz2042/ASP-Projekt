fa1f90f6211cc962249b02bd220e6714fb231d96
Mateusz2042 <mati_1401@o2.pl>
2017-04-12 13:52:07 +0200

    QR Code Generator

---
+++ QRCodeGenerator/QRCodeGenerator/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using System.Drawing.Imaging;
+
+namespace QRCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder encoder = new QRCodeEncoder();
+
+            Bitmap img = encoder.Encode(textBox1.Text + " " + textBox2.Text);
+
+            img.Save(@"C:\Users\"+ Environment.UserName +@"\Desktop\code.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
